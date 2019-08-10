ed6571b74210055baf16e2e9676d82abac3bf589
danvanhorn <dan.vanhorn17@gmail.com>
2019-08-09 21:22:32 -0700

    update now.json

--- api/encrypt.js
+++ api/encrypt.js
@@ -2,9 +2,9 @@ import fs from 'fs';
 import { createCipheriv } from 'crypto';
 import { json, send } from 'micro';
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   const { ALGORITHM, KEY, IV } = process.env;
-  const payload = await json(req);
+  const { payload } = await json(req);
   const cipher = createCipheriv(ALGORITHM, KEY, IV);
   const encryptedPayload = cipher.update(payload, 'utf-8', 'base-64');
   send(res, 200, encryptedPayload);
